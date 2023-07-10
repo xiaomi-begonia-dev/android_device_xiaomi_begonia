370f3cc99c1e91c5ca010b80b2dd884a5ce6c5e8
MrErenK <akbaseren4751@gmail.com>
2023-07-10 12:29:56 +0300

    begonia: Switch back to stock fp * From MIUI V12.5.8.0 RGGMIXM
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- aosp_begonia.mk
+++ aosp_begonia.mk
@@ -43,6 +43,8 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+BUILD_FINGERPRINT := Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.8.0.RGGMIXM:user/release-keys
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
